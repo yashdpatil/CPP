6a1676cf40bdc906c3bbbd6022efc8dedbb5fb28
Yash Patil <102399703+yashdpatil@users.noreply.github.com>
2023-09-29 22:28:55 +0530

    Update Upper-lower.cpp

--- Upper-lower.cpp
+++ Upper-lower.cpp
@@ -1,1 +1,23 @@
-
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+	{
+		string s; cin>>s;
+		for(int i=0;i<=s.size();i++)
+		{
+			if(i%2==0)
+			{
+				if(!(s[i]>='a' && s[i]<'z'))
+					s[i]+=32;
+			//	s[i]=tolower(s[i]);
+				
+			}
+			else
+			{
+					if(!(s[i]>='A' && s[i]<'Z'))
+					s[i]-=32;
+			//	s[i]=toupper(s[i]);
+			}
+		}
+		cout<<s;
+	}
