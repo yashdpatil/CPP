51b469047558df89608489584b8ea6f7dad46db4
Yash Patil <102399703+yashdpatil@users.noreply.github.com>
2023-09-29 22:27:23 +0530

    Update Vector.cpp

--- Vector.cpp
+++ Vector.cpp
@@ -1,1 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+		 void printvec(vector<int>v)
+{
+		for(int i =0; i<v.size();i++)
+		{
+		
+			if(v[i]>10){
+			
+			
+			cout<<i;
+		}
+			
+		}
+	}
+	int main()
+	{
+	
+			
+	
+	vector<int> v;
+	v.push_back(40);
+	v.push_back(60);
+//	v.push_back(70);
+	vector<int> ::iterator it =v.begin();
+//	cout<<(*it)<<endl;
+	cout<<*it++<<endl;
+	printvec(v);
+	}
 
